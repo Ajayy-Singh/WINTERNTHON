2ba9fbe27d94853298094448e527feb6a2f59cd8
harshwardhanahirwar <ahirwarharshwardhan2@gmail.com>
2026-01-11 17:41:16 +0530

    update and give improve design

--- Frontend/Bug_Slayers/src/App.tsx
+++ Frontend/Bug_Slayers/src/App.tsx
@@ -1,4 +1,43 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Register from "./pages/Register";
+// import Editor from "./pages/Editor";
+// import MyWritings from "./pages/MyWritings";
+// import ProtectedRoute from "./components/ProtectedRoute";
+
+// export default function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/" element={<Login />} />
+//         <Route path="/register" element={<Register />} />
+
+//         <Route
+//           path="/editor"
+//           element={
+//             <ProtectedRoute>
+//               <Editor />
+//             </ProtectedRoute>
+//           }
+//         />
+
+//         <Route
+//           path="/writings"
+//           element={
+//             <ProtectedRoute>
+//               <MyWritings />
+//             </ProtectedRoute>
+//           }
+//         />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Editor from "./pages/Editor";
@@ -9,23 +48,25 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
 
-        <Route
-          path="/editor"
-          element={
-            <ProtectedRoute>
-              <Editor />
-            </ProtectedRoute>
-          }
-        />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
         <Route
           path="/writings"
           element={
             <ProtectedRoute>
               <MyWritings />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/editor"
+          element={
+            <ProtectedRoute>
+              <Editor />
             </ProtectedRoute>
           }
         />
